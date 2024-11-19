47a86543f3c3a5d13e5a04e52f72bf34c0ea2196
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2024-11-19 00:38:36 -0700

    adds raw policy executor

---
+++ table_rl/explorers/policy_executor.py
@@ -0,0 +1,33 @@
+import numpy as np
+from table_rl import explorer
+from pdb import set_trace
+
+
+class PolicyExecutor(explorer.Explorer):
+    """Executes a specific policy
+
+    Args:
+      policy: 2D numpy array where policy[state,action] is the pi(a|s)
+    """
+
+    def __init__(self, policy):
+        self.policy = policy
+
+
+    def select_action(self, obs, action_values) -> int:
+        return np.random.choice(self.policy.shape[1], p=self.policy[obs])
+
+
+    def observe(self, obs, reward, terminated, truncated):
+        """Select an action.
+
+        Args:
+          obs: next state/observation
+          reward: reward received
+          terminated: bool indicating environment termination
+          truncated: bool indicating epsisode truncation
+        """
+        pass
+
+
+
