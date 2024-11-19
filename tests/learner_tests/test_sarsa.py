71780202514569cb09a728251f7be5a7f55c7d88
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2024-11-18 23:35:22 -0700

    Adds tests for termination and truncation

--- tests/learner_tests/test_sarsa.py
+++ tests/learner_tests/test_sarsa.py
@@ -61,3 +61,38 @@ class TestSARSA:
         expected_next_q = np.array([[expected_updated_q_value, 2], [3,4], [5,6]], dtype=float)
         agent.observe(1, mock_reward, False, False, training_mode=True)
         np.testing.assert_allclose(expected_next_q, agent.q)
+
+    def test_sarsa_update_termination(self):
+        explorer = table_rl.explorers.GreedyExplorer(self.T.shape[1])
+        agent = table_rl.learners.SARSA(self.T.shape[0],
+                          self.T.shape[1],
+                          0.1,
+                          explorer,
+                          discount=self.discount,
+                          initial_val=0.)
+        agent.q = np.array([[1,2], [3,4], [5,6]], dtype=float)
+        agent.current_obs = 0
+        agent.action = 0
+        mock_next_state = 1
+        mock_reward = 2.0
+        expected_updated_q_value = 1 + 0.1 * (mock_reward  - 1)
+        expected_next_q = np.array([[expected_updated_q_value, 2], [3,4], [5,6]], dtype=float)
+        agent.observe(1, mock_reward, True, False, training_mode=True)
+        np.testing.assert_allclose(expected_next_q, agent.q)
+
+
+    def test_sarsa_truncation(self):
+        explorer = table_rl.explorers.GreedyExplorer(self.T.shape[1])
+        agent = table_rl.learners.SARSA(self.T.shape[0],
+                          self.T.shape[1],
+                          0.1,
+                          explorer,
+                          discount=self.discount,
+                          initial_val=0.)
+        agent.current_obs = 0
+        agent.action = 0
+        agent.observe(1, 5, False, False, training_mode=True)
+        assert agent.next_action is not None
+        agent.observe(1, 5, False, True, training_mode=True)
+        assert agent.next_action is None
+
