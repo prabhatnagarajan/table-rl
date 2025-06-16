3aeda8590d03609a6ed0cb7dc1e75fc219b91a08
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2025-06-15 22:51:29 -0500

    Adds initial QV-learning code

---
+++ table_rl/learners/qv.py
@@ -0,0 +1,59 @@
+from table_rl import learner
+import numpy as np
+
+class QVLearning(learner.Learner):
+    """Class that implements QV-Learning."""
+
+    def __init__(self,
+                 num_states,
+                 num_actions,
+                 step_size_q_schedule,
+                 step_size_v_schedule,
+                 explorer,
+                 discount=0.99,
+                 initial_val=0.):
+        self.explorer = explorer
+        self.step_size_q_schedule = step_size_q_schedule
+        self.step_size_v_schedule = step_size_v_schedule
+        self.q = np.full((num_states, num_actions), initial_val, dtype=float)
+        self.v = np.full((num_states), initial_val, dtype=float)
+        self.discount = discount
+
+    def _update_q(self, obs, action, reward, terminated, next_obs, step_size):
+        target = reward if terminated else reward + self.discount * self.v[next_obs]
+        q_estimate = self.q[obs, action]
+        self.q[obs, action] = q_estimate + step_size * (target - q_estimate)
+
+    def _update_v(self, obs, action, reward, terminated, next_obs, step_size):
+        target = reward if terminated else reward + self.discount * self.v[next_obs]
+        v_estimate = self.v[obs]
+        self.v[obs] = v_estimate + step_size * (target - v_estimate)
+
+    def update(self, obs, action, reward, terminated, next_obs):
+        step_size = self.step_size_q_schedule.step_size(obs, 0)
+        self._update_q(obs, action, reward, terminated, next_obs, step_size)
+        self._update_v(obs, action, reward, terminated, next_obs, step_size)
+    
+    def act(self, obs: int, train: bool) -> int:
+        """Returns an integer 
+        """
+        self.current_obs = obs
+        q_values = self.q[obs]
+        action = self.explorer.select_action(obs, q_values) if train else np.argmax(q_values)
+        self.last_action = action
+        return action
+        
+    def observe(self, obs: int, reward: float, terminated: bool, truncated: bool, training_mode: bool) -> None:
+        """Observe consequences of the last action and update estimates accordingly.
+
+        Returns:
+            None
+        """
+        self.update(self.current_obs, self.last_action, reward, terminated, obs)
+        self.explorer.observe(obs, reward, terminated, truncated, training_mode)
+        self.step_size_q_schedule.observe(obs, reward, terminated, truncated, training_mode)
+        self.step_size_v_schedule.observe(obs, reward, terminated, truncated, training_mode)
+        if terminated or truncated:
+            self.current_obs = None
+            self.last_action = None
+
