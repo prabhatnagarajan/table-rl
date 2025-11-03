cb54c9c34a6d3b1d795948824a1533890892a6cb
Prabhat Nagarajan <prabhat.nagarajan@gmail.com>
2025-11-02 18:02:47 -0700

    Adds Expected Sarsa

--- tests/learner_tests/test_expected_sarsa.py
+++ tests/learner_tests/test_expected_sarsa.py
@@ -15,45 +15,46 @@ class TestExpectedSarsa:
         self.discount = 0.9
 
 
-    def test_q_learning_loop(self):
-        explorer = table_rl.explorers.ConstantEpsilonGreedy(0.1, self.T.shape[1])
+    def test_expected_sarsa_loop(self):
+        policy = np.array([[0.2, 0.8],[0.3, 0.7],[0.9, 0.1]])
+        explorer = table_rl.explorers.PolicyExecutor(policy)
 
-        agent = QLearning(self.T.shape[0],
+        agent = ExpectedSarsa(self.T.shape[0],
                           self.T.shape[1],
                           table_rl.step_size_schedulers.ConstantStepSize(0.02),
                           explorer,
                           discount=self.discount,
                           initial_val=0.)
 
-        observation, info = self.env.reset()
+        observation, _ = self.env.reset()
 
         for _ in range(300000):
             action = agent.act(observation, True)
-            observation, reward, terminated, truncated, info = self.env.step(action)
+            observation, reward, terminated, truncated, _ = self.env.step(action)
             agent.observe(observation, reward, terminated, truncated, training_mode=True)
             if terminated or truncated:
-                observation, info = self.env.reset()
+                observation, _ = self.env.reset()
 
-        hand_confirmed_opt = np.array([[0.79345359, 0.8708637 ], 
-                                       [0.80539959, 0.97837773],
-                                       [0.,         0.        ]])
-        np.testing.assert_almost_equal(hand_confirmed_opt, agent.q, decimal=2)
+        ground_truth = table_rl.dp.policy_q_evaluation(policy, self.R, self.T, self.discount, 10000)
+        np.testing.assert_almost_equal(ground_truth, agent.q, decimal=2)
 
-    def test_q_learning_update(self):
-        explorer = table_rl.explorers.ConstantEpsilonGreedy(0.1, self.T.shape[1])
-        agent = QLearning(self.T.shape[0],
+    def test_expected_sarsa_update(self):
+        policy = np.array([[0.2, 0.8],[0.3, 0.7],[0.9, 0.1]])
+        explorer = table_rl.explorers.PolicyExecutor(policy)
+        agent = ExpectedSarsa(self.T.shape[0],
                           self.T.shape[1],
                           table_rl.step_size_schedulers.ConstantStepSize(0.1),
                           explorer,
                           discount=self.discount,
                           initial_val=0.)
-        agent.q = np.array([[1,2], [3,4], [5,6]], dtype=float)
+        agent.q = np.array([[1,2], [3, 4], [5,6]], dtype=float)
         agent.current_obs = 0
         agent.last_action = 0
         mock_next_state = 1
         mock_reward = 2.0
-        expected_updated_q_value = 1 + 0.1 * (mock_reward + self.discount * 4  - 1)
+        target_q = 0.3 * 3 + 0.7 * 4
+        expected_updated_q_value = 1 + 0.1 * (mock_reward + self.discount * target_q - 1)
         expected_next_q = np.array([[expected_updated_q_value, 2], [3,4], [5,6]], dtype=float)
-        agent.observe(1, mock_reward, False, False, training_mode=True)
+        agent.observe(mock_next_state, mock_reward, False, False, training_mode=True)
         np.testing.assert_allclose(expected_next_q, agent.q)
 
